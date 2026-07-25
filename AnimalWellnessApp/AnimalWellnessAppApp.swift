e850333f565f195f3992f7fe346c767195ec2b36
cwyatt1206 <165800722+cwyatt1206@users.noreply.github.com>
2026-07-25 13:08:44 -0400

    Initial Commit

---
+++ AnimalWellnessApp/AnimalWellnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalWellnessAppApp.swift
+//  AnimalWellnessApp
+//
+//  Created by Cassondra Wyatt on 7/25/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimalWellnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
